6be2f201c02eff79e7d715df869768b12ffe665a
Phoebe-Emefa <phoebeemefa@gmail.com>
2023-05-22 10:41:55 +0000

    added custom color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#3b3f44',
+    },
+    primary: {
+      main: '#f78318',
+    },
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div>heey</div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
